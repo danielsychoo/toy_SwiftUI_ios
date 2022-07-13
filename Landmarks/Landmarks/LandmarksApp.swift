88cf5c632018a1bc5180b76daddb028a86146177
danielsychoo <sungyeop.choo@gmail.com>
2022-07-13 17:04:35 +0900

    Initial Commit

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by sungyeopTW on 2022/07/13.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
